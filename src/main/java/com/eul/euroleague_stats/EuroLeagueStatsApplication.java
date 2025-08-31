5571673bd1b27c05560294788a195dc4fe2b51fe
markovujic04 <116155074+MarkoVujic04@users.noreply.github.com>
2025-08-31 13:58:30 +0200

    Initial Backend Setup

---
+++ src/main/java/com/eul/euroleague_stats/EuroLeagueStatsApplication.java
@@ -0,0 +1,13 @@
+package com.eul.euroleague_stats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuroLeagueStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuroLeagueStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eul/euroleague_stats/EuroLeagueStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eul.euroleague_stats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuroLeagueStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
